9c036e9efef546c11131999bbff27ff3e37b3782
Michael Yang <themichaelyang@gmail.com>
2019-04-05 20:47:53 -0400

    Finish hw 7

--- hw-7.py
+++ hw-7.py
@@ -48,7 +48,7 @@ def make_polygon(corners, total_pts):
     return points
 
 
-# In[9]:
+# In[5]:
 
 
 def recur(fn, x_in, y_in, remaining):
@@ -100,13 +100,72 @@ def problem_1():
     ax2.scatter(trapezoid_f4_inv[0], trapezoid_f4_inv[1], s=1, color="dodgerblue")
 
 
-# In[10]:
+# In[6]:
 
 
 problem_1()
 
 
-# In[33]:
+# In[124]:
+
+
+# .plot is faster than .scatter for large # of data points
+def problem_1_fast():
+    a = 4
+    b = 0.2
+    
+    fig1, ax1 = plt.subplots(figsize=(8, 8))
+    fig2, ax2 = plt.subplots(figsize=(8, 8))
+    
+#     https://matplotlib.org/gallery/text_labels_and_annotations/custom_legends.html#sphx-glr-gallery-text-labels-and-annotations-custom-legends-py
+#     color_cycle = plt.get_cmap('Pastel2')(np.linspace(0, 1, 3))
+#     color_cycle = ['#E0BBE4', '#957DAD', '#D291BC']
+    color_cycle = ['#FFCDCD', '#FFC961', '#FF927A']
+    ax1.set_prop_cycle(color=color_cycle)
+    ax2.set_prop_cycle(color=color_cycle)
+    
+    ax1.set_aspect('equal')
+    ax1.set_ylim(-6, 6)
+    ax1.set_xlim(-6, 6)
+    
+    ax2.set_aspect('equal')
+    ax2.set_ylim(-6, 6)
+    ax2.set_xlim(-6, 6)
+    
+    trapezoid = make_polygon([[-3, -3], [3, -1.5], [3, 1.5], [-3, 3]], 10**6)
+    trapezoid_x = trapezoid[:,0].flatten()
+    trapezoid_y = trapezoid[:,1].flatten()
+    dist = trapezoid_x + trapezoid_y
+    
+    # Plot trapezoid, f1 and f1_inv
+    ax1.plot(trapezoid_x, trapezoid_y, 'o', markersize=.1)
+    
+    trapezoid_f1 = henon(a, b)(trapezoid_x, trapezoid_y)
+    ax1.plot(trapezoid_f1[0], trapezoid_f1[1], 'o', markersize=.1)
+    
+    trapezoid_f1_inv = henon_inv(a, b)(trapezoid_x, trapezoid_y)
+    ax1.plot(trapezoid_f1_inv[0], trapezoid_f1_inv[1], 'o', markersize=.1)
+    
+    # Plot trapezoid, f4 and f4_inv
+    ax2.plot(trapezoid_x, trapezoid_y, 'o', markersize=.1)
+    
+    henon4 = lambda a, b: lambda x, y: recur(henon(a, b), x, y, 4)
+    henon4_inv = lambda a, b: lambda x, y: recur(henon_inv(a, b), x, y, 4)
+    
+    trapezoid_f4 = henon4(a, b)(trapezoid_x, trapezoid_y)
+    trapezoid_f4_inv = henon4_inv(a, b)(trapezoid_x, trapezoid_y)
+    
+    ax2.plot(trapezoid_f4[0], trapezoid_f4[1], 'o', markersize=.1)
+    ax2.plot(trapezoid_f4_inv[0], trapezoid_f4_inv[1], marker='o', markersize=.1)
+
+
+# In[125]:
+
+
+problem_1_fast()
+
+
+# In[7]:
 
 
 import functools
@@ -181,20 +240,21 @@ def problem_2():
     ax.scatter(trajectory[:,0].flatten(), trajectory[:,1].flatten(), s=0.1, color="mediumpurple")
 
 
-# In[34]:
+# In[8]:
 
 
 problem_2()
+# scaling dimension: ln(3) / ln(2)
 
 
-# In[35]:
+# In[9]:
 
 
 # np.apply_along_axis(lambda line: print(line), 0, [[1, 2, 3], [4, 5, 6]])
 # np.apply_along_axis(lambda line: print(line), 1, [[1, 2, 3], [4, 5, 6]])
 
 
-# In[46]:
+# In[10]:
 
 
 # deterministic IFS
@@ -232,8 +292,87 @@ def problem_3():
     ax.scatter(trajectory[:,0].flatten(), trajectory[:,1].flatten(), s=0.1, color="mediumpurple")
 
 
-# In[47]:
+# In[11]:
 
 
 problem_3()
+
+
+# In[12]:
+
+
+def problem_4():
+    f1 = lambda x: np.array((x[0]/3, x[1]/3))
+    f2 = lambda x: np.array((x[0]/3 + 2/3, x[1]/3))
+    f3 = lambda x: np.array((x[0]/3, x[1]/3 + 2/3))
+    f4 = lambda x: np.array((x[0]/3 + 2/3, x[1]/3 + 2/3))
+    
+    ifs = make_ifs([f1, f2, f3, f4])
+    
+    trajectory = iterate_map_vec(ifs, [0.5, 0.5], 10**5)
+    
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_aspect('equal')
+    
+    ax.scatter(trajectory[:,0].flatten(), trajectory[:,1].flatten(), s=1)
+
+
+# In[13]:
+
+
+problem_4()
+
+
+# In[14]:
+
+
+def problem_5e():
+    c = (2 * np.sqrt(2))
+    f1 = lambda x: np.asarray([x[0]/c - x[1]/c + 1/4, x[0]/c + x[1]/c + 1/4])
+    f2 = lambda x: np.asarray([x[0]/2 + 1/2, x[1]/2])
+    
+    ifs = make_ifs([f1, f2])
+    
+    trajectory = iterate_map_vec(ifs, [0.5, 0.5], 10**5)
+    
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_aspect('equal')
+    
+    ax.scatter(trajectory[:,0].flatten(), trajectory[:,1].flatten(), s=0.1, alpha=0.8)
+
+
+# In[15]:
+
+
+problem_5e()
+
+
+# In[208]:
+
+
+def get_boxes(x, boxes):
+    return np.floor((x + abs(min(x))) * boxes)
+
+def problem_6():
+    fn = henon(1.4, 0.3)
+    trajectory = iterate_map(fn, [0.2, 0.2], 10**5 - 1)
+    traj_x = trajectory[:,0].flatten()
+    traj_y = trajectory[:,1].flatten()
+    
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.plot(traj_x, traj_y, 'o', markersize=0.1, color='#FF8686')
+    
+    boxes = 10**5
+    
+    box_coords = np.array([get_boxes(traj_x, boxes), get_boxes(traj_y, boxes)]).T
+    covering_boxes = len(np.unique(box_coords))
+    
+    print("Box counting dimension: ")
+    print(np.log(covering_boxes) / np.log(boxes))
+
+
+# In[209]:
+
+
+problem_6()
 
